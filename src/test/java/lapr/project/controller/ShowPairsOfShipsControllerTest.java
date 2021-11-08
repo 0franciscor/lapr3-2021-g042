3ba18e0b54a7c9b229cacf523a738f306bce914e
RitaAriana <1201386@isep.ipp.pt>
2021-11-08 15:57:12 +0000

    G42-9 #comment unitary tests

---
+++ src/test/java/lapr/project/controller/ShowPairsOfShipsControllerTest.java
@@ -0,0 +1,18 @@
+package lapr.project.controller;
+
+import lapr.project.model.ShipLocation;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class ShowPairsOfShipsControllerTest {
+
+    List<ShipLocation> arr = new ArrayList<>();
+
+    @Test
+    public void getPairsOfShip() {
+    }
+}

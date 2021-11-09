92ffa7f3cdbd1b9e3f0c0f24c50a3b4ee8046dc6
pedroccelestino <1201382@isep.ipp.pt>
2021-11-09 00:25:31 +0000

    #G42-81 #time 1h #comment Initiating the Report - Header, abstract & Introduction

--- src/test/java/lapr/project/mapper/dto/ShipDetailsDtoTest.java
+++ src/test/java/lapr/project/mapper/dto/ShipDetailsDtoTest.java
@@ -25,14 +25,6 @@ public class ShipDetailsDtoTest {
         Assert.assertEquals(ship1.getShipID(), "IMO2113432");
     }
 
-    /*@Test
-    public void getEnergyGenerators() {
-    }
-
-    @Test
-    public void getGeneratorOutput() {
-    }*/
-
     @Test
     public void getCallSign() {
         ShipDetailsDto ship1 = new ShipDetailsDto("211331640","SEOUL EXPRESS","IMO2113432","DHBN",70,294,32,13);
@@ -56,13 +48,7 @@ public class ShipDetailsDtoTest {
     public void getWidth() {
         ShipDetailsDto ship1 = new ShipDetailsDto("211331640","SEOUL EXPRESS","IMO2113432","DHBN",70,294,32,13);
         Assert.assertEquals(ship1.getWidth(), 32,0.0);
-
     }
-
-    /*@Test
-    public void getCapacity() {
-    }*/
-
     @Test
     public void getDraft() {
         ShipDetailsDto ship1 = new ShipDetailsDto("211331640","SEOUL EXPRESS","IMO2113432","DHBN",70,294,32,13);

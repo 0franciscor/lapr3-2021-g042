4109192b17cc9f258affb4550d8deee6e84bee2e
FranciscoRedol <1201239@isep.ipp.pt>
2021-11-10 14:38:31 +0000

    G42-7 #comment Testes added to the OrgRole class.

--- src/test/java/lapr/project/model/OrganizationRoleTest.java
+++ src/test/java/lapr/project/model/OrganizationRoleTest.java
@@ -12,8 +12,34 @@ class OrganizationRoleTest {
         assertEquals("Traffic Manager", organizationRole.getDesignation());
     }
 
+
+
     @Test
-    void testEquals() {
+    void testEqualsObject(){
+        OrganizationRole orgRole = new OrganizationRole("Traffic Manager");
+        OrganizationRole orgRole2 = orgRole;
+
+        assertTrue(orgRole.equals(orgRole2));
+    }
+
+    @Test
+    void testEqualsNull(){
+        OrganizationRole orgRole = new OrganizationRole("Traffic Manager");
+        OrganizationRole orgRole2 = null;
+
+        assertFalse(orgRole.equals(orgRole2));
+    }
+
+    @Test
+    void testEqualsClass(){
+        OrganizationRole orgRole = new OrganizationRole("Traffic Manager");
+        String testString = "abc";
+
+        assertFalse(orgRole.equals(testString));
+    }
+
+    @Test
+    void testEqualsCorrect() {
         OrganizationRole organizationRole = new OrganizationRole("Traffic Manager");
         OrganizationRole organizationRole2 = new OrganizationRole("Traffic Manager");
         assertEquals(organizationRole, organizationRole2);

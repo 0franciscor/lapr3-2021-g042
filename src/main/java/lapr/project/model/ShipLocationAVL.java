0fc030aa8df94930cb141841ecd4929a2a7f3d8e
FranciscoRedol <1201239@isep.ipp.pt>
2021-11-11 01:44:24 +0000

    G42-1 #comment  AVL Testing

--- src/main/java/lapr/project/model/ShipLocationAVL.java
+++ src/main/java/lapr/project/model/ShipLocationAVL.java
@@ -57,6 +57,8 @@ public class ShipLocationAVL extends ShipLocationBST<ShipLocation>{
 
     @Override
     public void insert(ShipLocation element){
+        if(element == null)
+            return;
         root = insert(element, root);
     }
 

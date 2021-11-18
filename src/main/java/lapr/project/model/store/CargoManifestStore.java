fda9d63a4d2838276dfe62fc5ca34eb8d666f07d
Manuela Leite <1200720@isep.ipp.pt>
2021-11-18 11:11:00 +0000

    Solvig the build problem in Jenkinks

---
+++ src/main/java/lapr/project/model/store/CargoManifestStore.java
@@ -0,0 +1,18 @@
+package lapr.project.model.store;
+
+import lapr.project.model.CargoManifest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CargoManifestStore {
+
+    private List<CargoManifest> cargoManifestList;
+
+    public CargoManifestStore(){
+        cargoManifestList = new ArrayList<>();
+    }
+
+
+
+}

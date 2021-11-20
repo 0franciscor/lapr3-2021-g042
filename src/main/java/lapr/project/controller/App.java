05dc28fa0060c3288c3375a46be9ce8a0108b15e
FranciscoRedol <1201239@isep.ipp.pt>
2021-11-20 23:23:03 +0000

    G42-60 #time 1h DataBase Connection on App.

--- src/main/java/lapr/project/controller/App.java
+++ src/main/java/lapr/project/controller/App.java
@@ -1,11 +1,20 @@
 package lapr.project.controller;
+
+import lapr.project.data.ConnectionFactory;
+import lapr.project.data.DatabaseConnection;
 import lapr.project.model.Company;
+import lapr.project.model.ImportShip;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 /**
  * Class that represents a app.
  *
  * @author Rita Ariana Sobral <1201386@isep.ipp.pt>
+ * @author Francisco Redol <1201239@isep.ipp.pt>
  */
 public class App {
 
@@ -14,17 +23,32 @@ public class App {
      */
     private final Company company;
 
+    /**
+     * Represents the App's connection to the database
+     */
+    private final DatabaseConnection databaseConnection;
+
     private App(){
         company = new Company();
+        this.databaseConnection = initializeConnection();
     }
 
     /**
-     * Allows the user to get the instance of the running App.
-     * @return the instance of the running App
+     * Allows the user to get the instance of the running App's company.
+     * @return the instance of the running App company
      */
 
     public Company getCompany(){
         return company;
+    }
+
+
+    /**
+     * Allows the user to get the instance of the running App's database connection.
+     * @return the App's database connection
+     */
+    public DatabaseConnection getDataBaseConnection(){
+        return databaseConnection;
     }
 
     //############# Singleton #############
@@ -38,5 +62,22 @@ public class App {
         }
 
         return singleton;
+    }
+
+    /**
+     * Method responsible for initializing the database connection.
+     * @return the database connection
+     */
+    public DatabaseConnection initializeConnection(){
+        DatabaseConnection databaseConnection = null;
+        try {
+            databaseConnection = ConnectionFactory.getInstance()
+                    .getDatabaseConnection();
+        } catch (IOException exception) {
+            Logger.getLogger(ImportShip.class.getName())
+                    .log(Level.SEVERE, null, exception);
+        }
+
+        return databaseConnection;
     }
 }

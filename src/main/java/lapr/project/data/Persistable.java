a73c77802ddca107da447d650718b7106b2311f4
FranciscoRedol <1201239@isep.ipp.pt>
2021-11-20 23:22:05 +0000

    G42-60 #time 6h30 Time adjusted due to Previous Commits. Interface implemented

---
+++ src/main/java/lapr/project/data/Persistable.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lapr.project.data;
+
+/**
+ * @author nunocastro
+ */
+public interface Persistable {
+    /**
+     * Save an objet to the database.
+     *
+     * @param databaseConnection
+     * @param object
+     * @return Operation success.
+     */
+    boolean save(DatabaseConnection databaseConnection, Object object);
+
+    /**
+     * Delete an object from the database.
+     *
+     * @param databaseConnection
+     * @param object
+     * @return Operation success.
+     */
+    boolean delete(DatabaseConnection databaseConnection, Object object);
+
+    /**
+     * Get an object from the database.
+     *
+     * @param databaseConnection
+     * @param code
+     * @return Operation success
+     */
+    boolean getObject(DatabaseConnection databaseConnection, String code);
+
+    //TODO: não faltará aqui uma operação para obter um objeto da base de dados? nao
+}

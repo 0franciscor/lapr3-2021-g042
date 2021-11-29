8f32d897c5e556eff4e54fdc17517a376f19da14
FranciscoRedol <1201239@isep.ipp.pt>
2021-11-29 11:25:30 +0000

    G42-60 #time 3h Port, PlaceLocation and Countries Importing.

--- src/main/java/lapr/project/data/SendToDatabase.java
+++ src/main/java/lapr/project/data/SendToDatabase.java
@@ -1,11 +1,11 @@
 package lapr.project.data;
 
 import lapr.project.controller.App;
-import lapr.project.model.Ship;
-import lapr.project.model.ShipLocation;
-import lapr.project.model.Ports;
+import lapr.project.model.*;
 
 import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class SendToDatabase implements Persistable {
 
@@ -34,8 +34,13 @@ public class SendToDatabase implements Persistable {
         }
     }
 
+    /**
+     * Method responsible for saving Ports, PlaceLocations and Countries to the database
+     */
     public void sendPortsToDatabase(){
-
+        //for(Object objectPort : App.getInstance().getCompany().getPortStr().getPorts2DTree().)
+        Ports port = new Ports(new Country("Europa","Portugal"), 325, "Porto de leixoes", new PlaceLocation(41.18322878077638, -8.703141533061505));
+        savePort(databaseConnection, port);
     }
 
     //################################# SHIP RELATED #####################################
@@ -128,7 +133,6 @@ public class SendToDatabase implements Persistable {
         executeShipStatementOnDatabase(databaseConnection, ship, sqlCommand);
     }
 
-
     /**
      * Adds a new ship record to the database.
      *
@@ -143,7 +147,6 @@ public class SendToDatabase implements Persistable {
 
         executeShipStatementOnDatabase(databaseConnection, ship, sqlCommand);
     }
-
 
     /**
      * Executes the save Ship Statement.
@@ -172,8 +175,9 @@ public class SendToDatabase implements Persistable {
 
         try{
             saveShipPreparedStatement.executeUpdate();
-        } catch (Exception e){
+        } catch (SQLException e){
             System.out.println("There was an error when importing the Ship with the " + ship.getMMSI() + " MMSI code.");
+            databaseConnection.registerError(e);
         } finally {
             saveShipPreparedStatement.close();
         }
@@ -194,12 +198,13 @@ public class SendToDatabase implements Persistable {
         boolean returnValue;
 
         try {
-            saveLocationToDatabase(databaseConnection, shipLocation);
+            savePositionToDatabase(databaseConnection, shipLocation);
             returnValue = true;
 
-        } catch (SQLException ex) {
+        } catch (SQLException e) {
             returnValue = false;
             System.out.println("There was an error when importing a Ship Location to the database.");
+            databaseConnection.registerError(e);
         }
         return returnValue;
     }
@@ -212,10 +217,12 @@ public class SendToDatabase implements Persistable {
      * @param shipLocation that contains the location of a ship
      * @throws SQLException in case an error with the database occurs
      */
-    private void saveLocationToDatabase(DatabaseConnection databaseConnection, ShipLocation shipLocation)
+    private void savePositionToDatabase(DatabaseConnection databaseConnection, ShipLocation shipLocation)
             throws SQLException {
-        if (!isLocationOnDatabase(databaseConnection, shipLocation))
-            insertLocationOnDatabase(databaseConnection, shipLocation);
+        if (isPositionOnDatabase(databaseConnection, shipLocation))
+            updatePositionOnDatabase(databaseConnection, shipLocation);
+        else
+            insertPositionOnDatabase(databaseConnection, shipLocation);
     }
 
     /**
@@ -226,7 +233,7 @@ public class SendToDatabase implements Persistable {
      * @return if a location exists on the database
      * @throws SQLException in case an error with the database occurs
      */
-    private boolean isLocationOnDatabase(DatabaseConnection databaseConnection, ShipLocation shipLocation)
+    private boolean isPositionOnDatabase(DatabaseConnection databaseConnection, ShipLocation shipLocation)
             throws SQLException {
 
         Connection connection = databaseConnection.getConnection();
@@ -252,50 +259,81 @@ public class SendToDatabase implements Persistable {
     }
 
     /**
+     * Updates an existing Position record on the database.
+     *
+     * @param databaseConnection to the database
+     * @param shipLocation that is related to the database
+     * @throws SQLException in case an error with the database occurs
+     */
+    private void updatePositionOnDatabase(DatabaseConnection databaseConnection, ShipLocation shipLocation)
+            throws SQLException {
+        String sqlCommand =
+                "update ShipPosition set latitude = ?, longitude = ?, sog = ?, cog = ?, heading = ?, position = ?, transceiver = ? where shipMmsiCode = ? and baseDateTime = ?";
+
+        executeShipPositionStatementOnDatabase(databaseConnection, shipLocation, sqlCommand, false);
+    }
+
+    /**
      * Adds a new shipLocation record to the database.
      *
      * @param databaseConnection to the database
      * @param shipLocation that is related to the database
      * @throws SQLException in case an error with the database occurs
      */
-    private void insertLocationOnDatabase(DatabaseConnection databaseConnection, ShipLocation shipLocation)
+    private void insertPositionOnDatabase(DatabaseConnection databaseConnection, ShipLocation shipLocation)
             throws SQLException {
 
         String sqlCommand = "insert into ShipPosition(shipMmsiCode, baseDateTime, latitude, longitude, sog, cog, heading, position, transceiver) values (?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
-        executeShipLocationStatementOnDatabase(databaseConnection, shipLocation, sqlCommand);
+        executeShipPositionStatementOnDatabase(databaseConnection, shipLocation, sqlCommand, true);
     }
 
     /**
      * Executes the save ShipLocation Statement.
-     *
+     * @param databaseConnection connection to the database
      * @param shipLocation that is related to the database
+     * @param type of insert that is being made
      * @throws SQLException in case an error with the database occurs
      */
-    private void executeShipLocationStatementOnDatabase(DatabaseConnection databaseConnection, ShipLocation shipLocation, String sqlCommand)
+    private void executeShipPositionStatementOnDatabase(DatabaseConnection databaseConnection, ShipLocation shipLocation, String sqlCommand, boolean type)
             throws SQLException {
 
         Connection connection = databaseConnection.getConnection();
         PreparedStatement getShipLocationPreparedStatement = connection.prepareStatement(sqlCommand);
 
-        getShipLocationPreparedStatement.setString(1, shipLocation.getMMSI());
-        getShipLocationPreparedStatement.setTimestamp(2, new Timestamp(shipLocation.getMessageTime().getTime()));
-        getShipLocationPreparedStatement.setString(3, shipLocation.getLatitude());
-        getShipLocationPreparedStatement.setString(4, shipLocation.getLongitude());
-        getShipLocationPreparedStatement.setFloat(5, shipLocation.getSOG());
-        getShipLocationPreparedStatement.setString(7, shipLocation.getHeading());
-        getShipLocationPreparedStatement.setFloat(6, shipLocation.getCOG());
-        getShipLocationPreparedStatement.setString(8, shipLocation.getPosition());
-        getShipLocationPreparedStatement.setString(9, shipLocation.getTransceiverClass());
+        if(type) {
+            getShipLocationPreparedStatement.setString(1, shipLocation.getMMSI());
+            getShipLocationPreparedStatement.setTimestamp(2, new Timestamp(shipLocation.getMessageTime().getTime()));
+            getShipLocationPreparedStatement.setString(3, shipLocation.getLatitude());
+            getShipLocationPreparedStatement.setString(4, shipLocation.getLongitude());
+            getShipLocationPreparedStatement.setFloat(5, shipLocation.getSOG());
+            getShipLocationPreparedStatement.setFloat(6, shipLocation.getCOG());
+            getShipLocationPreparedStatement.setString(7, shipLocation.getHeading());
+            getShipLocationPreparedStatement.setString(8, shipLocation.getPosition());
+            getShipLocationPreparedStatement.setString(9, shipLocation.getTransceiverClass());
+        } else {
+            getShipLocationPreparedStatement.setString(1, shipLocation.getLatitude());
+            getShipLocationPreparedStatement.setString(2, shipLocation.getLongitude());
+            getShipLocationPreparedStatement.setFloat(3, shipLocation.getSOG());
+            getShipLocationPreparedStatement.setFloat(4, shipLocation.getCOG());
+            getShipLocationPreparedStatement.setString(5, shipLocation.getHeading());
+            getShipLocationPreparedStatement.setString(6, shipLocation.getPosition());
+            getShipLocationPreparedStatement.setString(7, shipLocation.getTransceiverClass());
+            getShipLocationPreparedStatement.setString(8, shipLocation.getMMSI());
+            getShipLocationPreparedStatement.setTimestamp(9, new Timestamp(shipLocation.getMessageTime().getTime()));
+        }
 
         try {
             getShipLocationPreparedStatement.executeUpdate();
-        } catch (Exception e){
-            System.out.println("There was an error when importing the ShipLocation with " + shipLocation.getMMSI() + " code.");
+        } catch (SQLException e){
+            System.out.println("There was an error related to the ShipLocation with the " + shipLocation.getMMSI() + " MMSI code.");
+            databaseConnection.registerError(e);
         }finally {
             getShipLocationPreparedStatement.close();
         }
     }
+
+    //################################# Port Related ###########################################
 
     /**
      * Method responsible for saving an object to the database.
@@ -331,6 +369,13 @@ public class SendToDatabase implements Persistable {
      */
     private void savePortToDatabase(DatabaseConnection databaseConnection, Ports port)
             throws SQLException {
+
+        if(!isCountryOnDatabase(databaseConnection, port))
+            insertCountryOnDatabase(databaseConnection, port);
+
+        if(!isPlaceLocationOnDatabase(databaseConnection, port))
+            insertPlaceLocationOnDatabase(databaseConnection, port);
+
         if (isPortOnDatabase(databaseConnection, port))
             updatePortOnDatabase(databaseConnection, port);
         else
@@ -385,7 +430,6 @@ public class SendToDatabase implements Persistable {
         executePortStatementOnDatabase(databaseConnection, port, sqlCommand);
     }
 
-
     /**
      * Adds a new port record to the database.
      *
@@ -400,7 +444,6 @@ public class SendToDatabase implements Persistable {
 
         executePortStatementOnDatabase(databaseConnection, port, sqlCommand);
     }
-
 
     /**
      * Executes the save Port Statement.
@@ -417,15 +460,158 @@ public class SendToDatabase implements Persistable {
         PreparedStatement savePortPreparedStatement = connection.prepareStatement(sqlCommand);
 
         savePortPreparedStatement.setString(1, port.getPortName());
-        savePortPreparedStatement.setDouble(2, port.getLatitude());
-        savePortPreparedStatement.setDouble(3, port.getLongitude());
+        savePortPreparedStatement.setString(2, String.format("%.2f", port.getLatitude()));
+        savePortPreparedStatement.setString(3, String.format("%.2f", port.getLongitude()));
 
         try{
             savePortPreparedStatement.executeUpdate();
-        } catch (Exception e){
+        } catch (SQLException e){
             System.out.println("There was an error when importing the Port with the " + port.getCode() + " code.");
+            databaseConnection.registerError(e);
         } finally {
             savePortPreparedStatement.close();
+        }
+    }
+
+    //################################# Place Location Related ###########################################
+
+    /**
+     * Method that serves the purpose of verifying if a certain Place Location exists on the database.
+     *
+     * @param databaseConnection to the database
+     * @param port that contains the Place Location Data
+     * @return if the Place Location is on the database
+     *
+     * @throws SQLException in case an error with the database occurs
+     */
+    private boolean isPlaceLocationOnDatabase(DatabaseConnection databaseConnection, Ports port)
+            throws SQLException{
+
+        Connection connection = databaseConnection.getConnection();
+
+        boolean isPlaceLocationOnDatabase;
+
+        String sqlCommand = "select * from PlaceLocation where countryName = ? and latitude = ? and longitude = ?";
+
+        PreparedStatement getPlaceLocationPreparedStatement = connection.prepareStatement(sqlCommand);
+
+        getPlaceLocationPreparedStatement.setString(1, port.getCountryName());
+        getPlaceLocationPreparedStatement.setString(2, String.format("%.2f", port.getLatitude()));
+        getPlaceLocationPreparedStatement.setString(3, String.format("%.2f", port.getLongitude()));
+
+        try (ResultSet placeLocationResultSet = getPlaceLocationPreparedStatement.executeQuery()) {
+
+            isPlaceLocationOnDatabase = placeLocationResultSet.next();
+            placeLocationResultSet.close();
+
+        } finally {
+            getPlaceLocationPreparedStatement.close();
+        }
+        return isPlaceLocationOnDatabase;
+    }
+
+    /**
+     * Method responsible for inserting a certain Place Location on the database.
+     *
+     * @param databaseConnection to the database
+     * @param port that contains the Place Location Data
+     * @throws SQLException in case an error with the database occurs
+     */
+    private void insertPlaceLocationOnDatabase(DatabaseConnection databaseConnection, Ports port)
+            throws SQLException {
+        String sqlCommand =
+                "insert into PlaceLocation(countryName, latitude, longitude) values (?, ?, ?)";
+
+        executePlaceLocationStatementOnDatabase(databaseConnection, port, sqlCommand);
+    }
+
+    /**
+     * Method responsible for executing the PlaceLocationPreparedStatement
+     *
+     * @param databaseConnection to the database
+     * @param port that contains the Place Location Data
+     * @param sqlCommand that is going to be executed on the database
+     */
+    private void executePlaceLocationStatementOnDatabase(DatabaseConnection databaseConnection, Ports port, String sqlCommand)
+            throws SQLException {
+
+        Connection connection = databaseConnection.getConnection();
+
+        PreparedStatement savePlaceLocationPreparedStatement = connection.prepareStatement(sqlCommand);
+
+        savePlaceLocationPreparedStatement.setString(1, port.getCountryName());
+        savePlaceLocationPreparedStatement.setString(2, String.format("%.2f", port.getLatitude()));
+        savePlaceLocationPreparedStatement.setString(3, String.format("%.2f", port.getLongitude()));
+
+        try{
+            savePlaceLocationPreparedStatement.executeUpdate();
+        } catch (SQLException e){
+            System.out.println("There was an error when importing the PlaceLocation with " + port.getLatitude() + " latitude and " + port.getLongitude() + " longitude.");
+            databaseConnection.registerError(e);
+        } finally {
+            savePlaceLocationPreparedStatement.close();
+        }
+    }
+
+    //####################################### Country Related ###################################
+
+    /**
+     * Method that verifies if a certain country is on the database.
+     *
+     * @param databaseConnection to the database
+     * @param port that contains the Place Location Data
+     * @return if the selected Country is on the database
+     * @throws SQLException in case an error with the database occurs
+     */
+    private boolean isCountryOnDatabase(DatabaseConnection databaseConnection, Ports port)
+            throws SQLException{
+
+        Connection connection = databaseConnection.getConnection();
+
+        boolean isCountryOnDatabase;
+
+        String sqlCommand = "select * from Country where countryName = ?";
+
+        PreparedStatement getCountryPreparedStatement = connection.prepareStatement(sqlCommand);
+
+        getCountryPreparedStatement.setString(1, port.getCountryName());
+
+        try (ResultSet placeLocationResultSet = getCountryPreparedStatement.executeQuery()) {
+
+            isCountryOnDatabase = placeLocationResultSet.next();
+            placeLocationResultSet.close();
+
+        } finally {
+            getCountryPreparedStatement.close();
+        }
+        return isCountryOnDatabase;
+    }
+
+    /**
+     * Method that inserts a certain Country on the database.
+     *
+     * @param databaseConnection to the database
+     * @param port that contains the Place Location Data
+     * @throws SQLException in case an error with the database occurs
+     */
+    private void insertCountryOnDatabase(DatabaseConnection databaseConnection, Ports port)
+            throws SQLException{
+
+        Connection connection = databaseConnection.getConnection();
+
+        String sqlCommand = "insert into Country (countryName) values (?)";
+
+        PreparedStatement saveCountryPreparedStatement = connection.prepareStatement(sqlCommand);
+
+        saveCountryPreparedStatement.setString(1, port.getCountryName());
+
+        try{
+            saveCountryPreparedStatement.executeUpdate();
+        } catch (SQLException e){
+            System.out.println("There was an error when importing the Country with the " + port.getCountryName() + ".");
+            databaseConnection.registerError(e);
+        } finally {
+            saveCountryPreparedStatement.close();
         }
     }
 }

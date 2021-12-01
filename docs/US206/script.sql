dc8e7b7a08b43ae1d7049294a3f36871ef894c52
pedroccelestino <1201382@isep.ipp.pt>
2021-12-01 23:40:51 +0000

    G42-132 #time 45m #Comment Readjustments on the script for US206 and doing the connection between the script and Java

--- docs/US206/script.sql
+++ docs/US206/script.sql
@@ -1,65 +1,68 @@
-Declare
+CREATE OR REPLACE PROCEDURE US206 (mmsiCode in Varchar, outString out Varchar)
+IS
     cmunload Integer;
     bool Boolean;
-    cmload Integer;
+    phases Integer;
     cmcode CargoManifestLoad.id%type;
     nextLocatiON Phases.origin%type;
     cargoId Integer;
     iso_type varchar(255);
     weight_load float;
 
-Cursor cm IS
+CURSOR cm IS
         SELECT id
         FROM CargoManifestLoad
-        WHERE ShipmmsiCode ='210950000';
+        WHERE Shipmmsicode=mmsiCode;
 
-Begin
-open cm;
+BEGIN
+OPEN cm;
     LOOP 
         fetch cm INTO cmcode;
         Exit When cm%notfound;
         dbms_output.put_line('cargo id: ' ||cmcode);
-        SELECT Count(CARGOMANIFESTUNLOAD.PHASESCargoManifestLoadID) INTO cmunload 
-                FROM CARGOMANIFESTUNLOAD
-                INNER JOIN CargoManifestLoad
-                ON(CARGOMANIFESTUNLOAD.PHASESCargoManifestLoadID= CargoManifestLoad.id)
-                WHERE CARGOMANIFESTUNLOAD.PHASESCargoManifestLoadID = cmcode;                                          
-    
-                dbms_output.put_line(cmunload);
-    
-                SELECT COUNT(Phases.CargoManifestLoadid) INTO cmload 
-                FROM Phases
-                INNER JOIN CargoManifestLoad
-                ON(CargoManifestLoad.ID=Phases.CargoManifestLoadid)
-                WHERE Phases.CargoManifestLoadid = cmcode;
+        
+
+        SELECT COUNT(Phases.CargoManifestLoadid) INTO phases 
+        FROM Phases
+        INNER JOIN CargoManifestLoad
+        ON(CargoManifestLoad.ID=Phases.CargoManifestLoadid)
+        WHERE Phases.CargoManifestLoadid=cmcode;
+        dbms_output.put_line(phases);   
+
+        SELECT COUNT(CARGOMANIFESTUNLOAD.PHASESCargoManifestLoadID) INTO cmunload 
+        FROM CARGOMANIFESTUNLOAD
+        INNER JOIN CargoManifestLoad
+        ON(CARGOMANIFESTUNLOAD.PHASESCargoManifestLoadID=CargoManifestLoad.id)
+        WHERE CARGOMANIFESTUNLOAD.PHASESCargoManifestLoadID=cmcode;                                          
+        dbms_output.put_line(cmunload);
+
+        IF phases=cmunload THEN
+            dbms_output.put_line('Finalized cargo manifest id: '||cmcode);
                 
-                dbms_output.put_line(cmload);    
-                
-                IF cmload= cmunload THEN
-                        dbms_output.put_line('Finalized cargo manifest id: ' ||cmcode);
-                        
-                ELSE
-                    SELECT origin INTO nextLocatiON
-                    FROM Phases                    
-                    WHERE id=cmunload+1
-                    AND CargoManifestLoadid= cmcode;
-                    dbms_output.put_line('Next port: ' ||nextLocatiON);
-                   
-                    FOR teste 
-                    IN(SELECT Phases.CargoManifestLoadid 
-                    FROM Phases 
-                    INNER JOIN CargoManifestLoad 
-                    ON (Phases.CargoManifestLoadid=CargoManifestLoad.id)  
-                    WHERE Phases.origin = nextLocatiON 
-                    AND Phases.id=1 AND Phases.CargoManifestLoadid> 
-                    cmcode
-                    AND CargoManifestLoad.ShipmmsiCode ='210950000')
-                    LOOP
-                        SELECT isoCode, weight INTO iso_type, weight_load FROM CONtainer WHERE numberId=teste.CONtainerNumberId;
-                        dbms_output.put_line('Cargo Manifest of Containers to load: ' ||teste.CargoManifestLoadid ||'Type: ' ||iso_type ||'Load: ' ||weight_load);
-                    END LOOP;
-                        
-                END IF;
-                
+        ELSE
+            SELECT origin INTO nextLocatiON
+            FROM Phases                    
+            WHERE id=cmunload+1
+            AND CargoManifestLoadid= cmcode;
+            dbms_output.put_line('Next port: '||nextLocatiON);
+            
+            FOR teste 
+            IN(SELECT Phases.CargoManifestLoadid 
+            FROM Phases 
+            INNER JOIN CargoManifestLoad 
+            ON (Phases.CargoManifestLoadid=CargoManifestLoad.id)  
+            WHERE Phases.origin = nextLocatiON 
+            AND Phases.id=1 AND Phases.CargoManifestLoadid> 
+            cmcode
+            AND CargoManifestLoad.ShipmmsiCode = mmsiCode)
+            LOOP
+                SELECT isoCode, weight INTO iso_type, weight_load FROM CONtainer WHERE numberId=teste.CONtainerNumberId;
+                dbms_output.put_line('Cargo Manifest of Containers to load: ' ||teste.CargoManifestLoadid ||'Type: ' ||iso_type ||'Load: ' ||weight_load);
+                outString:=outString || 'Cargo Manifest of Containers to load: ' ||teste.CargoManifestLoadid ||'Type: ' ||iso_type ||'Load: ' ||weight_load || chr(10);
             END LOOP;
+                
+        END IF;
+        
+    END LOOP;
+    CLOSE cm;
 END;
---
+++ src/main/java/lapr/project/data/US206.java
@@ -0,0 +1,46 @@
+package lapr.project.data;
+
+import lapr.project.controller.App;
+import lapr.project.utils.WriteForAFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Types;
+
+public class US206 {
+
+    private Connection databaseConnection;
+    private String listOfContainers;
+    private WriteForAFile writeForAFile;
+
+    public US206(String mmsiCode) throws SQLException, IOException {
+        databaseConnection = App.getInstance().getDatabaseConnection().getConnection();
+        writeForAFile = new WriteForAFile();
+        initialize(mmsiCode);
+    }
+
+    private void initialize(String mmsiCode) throws SQLException, IOException {
+        CallableStatement statement = databaseConnection.prepareCall("{CALL US206(?, ?)}");
+        statement.registerOutParameter(2, Types.INTEGER);
+
+        statement.setString(1, mmsiCode);
+
+        statement.execute();
+
+        this.listOfContainers=statement.getString(2);
+
+        writeForAFile.writeForAFile(toString(), "US206_" + mmsiCode, new File("target\\generated-sources\\annotations\\US206"));
+        statement.close();
+        databaseConnection.close();
+    }
+
+    @Override
+    public String toString() {
+        return "US206{" +
+                "listOfContainers='" + listOfContainers + '\'' +
+                '}';
+    }
+}

d131544c432bff1466ceabccc6edc13e41546c22
RitaAriana <1201386@isep.ipp.pt>
2021-12-03 01:54:02 +0000

    G42-85 #time 2h #comment correction of some parts of the script and inserts for correct testing

--- docs/US204/script.sql
+++ docs/US204/script.sql
@@ -1,7 +1,42 @@
+insert into Container (NUMBERid, CHECKDIGIT, ISOCODE, MaxWeight, MAXVOLUMEPACKED, REPAIRRECOMMENDATION, CERTIFICATE, payload, tare, weight, maxweightpacked) values (987650321, 1569483, 'justo', 2.4, 1.5, '#REPAIRRECOMMENDATION', 'CERTIFICATE', 89.9, 1.1, 181.7, 118.5);
+insert into Container (NUMBERid, CHECKDIGIT, ISOCODE, MaxWeight, MAXVOLUMEPACKED, REPAIRRECOMMENDATION, CERTIFICATE, payload, tare, weight, maxweightpacked) values (695421863, 8150283, 'sagittis', 92.3, 179.3, '#REPAIRRECOMMENDATION', 'CERTIFICATE', 89.6, 20.7, 128.5, 72.1);
+
+insert into Ship (mmsicode, imocode, NUMBERENERGYGENERATORS, generatoroutput, callsign, draft, shipname, vesseltypeid, shiplength, width, cargo) values ('529467097', 'IMO4419462', 82, 16.8, 'sit', 39.1, 'consequat', 100, 72.2, 61.5, 'in');
+insert into Ship (mmsicode, imocode, NUMBERENERGYGENERATORS, generatoroutput, callsign, draft, shipname, vesseltypeid, shiplength, width, cargo) values ('696134988', 'IMO8583481', 57, 20.4, 'abc', 85.6, 'a', 17, 30.5, 16.9, 'magna');
+
+insert into Country (countryname, continent) values ('Indonesia', 'Continent');
+insert into Country (countryname, continent) values ('Libya', 'Continent');
+insert into Country (countryname, continent) values ('China', 'Continent');
+insert into Country (countryname, continent) values ('Montenegro', 'Continent');
+
+insert into PlaceLocation (countryname, latitude, longitude) values ('Indonesia', '70', '-93');
+insert into PlaceLocation (countryname, latitude, longitude) values ('Libya', '61', '41');
+insert into PlaceLocation (countryname, latitude, longitude) values ('China', '62', '145');
+insert into PlaceLocation (countryname, latitude, longitude) values ('Montenegro', '-16', '-99');
+
+insert into PORTS (id, name, PLACELOCATIONLATITUDE, PLACELOCATIONlongitude) values (117638542, 'sapien', '70', '-93');
+insert into PORTS (id, name, PLACELOCATIONLATITUDE, PLACELOCATIONlongitude) values (29536752, 'eget', '61', '41');
+insert into PORTS (id, name, PLACELOCATIONLATITUDE, PLACELOCATIONlongitude) values (937194604, 'nec', '62', '145');
+
+insert into CARGOMANIFESTLOAD(id,shipmmsicode,portid) values (10000,'529467097', 117638542);
+insert into CARGOMANIFESTLOAD(id,shipmmsicode,portid) values (20000,'696134988', 29536752);
+
+INSERT INTO PHASES(cargomanifestloadid,id,origin,destination,expecteddeparturedate,expectedarrivaldate,realdeparturedate,realarrivaldate)
+VALUES (20000,1,'eget','nec','2021-11-04 18:44:33','2021-12-30 18:44:33','2021-11-04 18:44:33','2021-12-30 18:44:33');
+
+INSERT INTO cargomanifestunload(id,phasescargomanifestloadid,phasesid,portid)
+VALUES (951245368,20000,1,937194604);
+
+INSERT INTO cargoManifestContainer(cargomanifestid,containernumberid,grosscontainer,xcontainer,ycontainer,zcontainer,PhasesId,PhasesCargoManifestLoadId )
+VALUES (10000,987650321,12.6,1,1,1,1,10000);
+
+INSERT INTO cargoManifestContainer(cargomanifestid,containernumberid,grosscontainer,xcontainer,ycontainer,zcontainer,PhasesId,PhasesCargoManifestLoadId,CargoManifestUnloadId)
+VALUES (20000,695421863,12.6,1,3,1,1,20000,951245368);
+
 CREATE OR REPLACE FUNCTION get_container_position (v_numberId int) return varchar
 
 IS
-    
+
     v_idCargoManifest int;
     v_CargoManifestUnloadId int;
     v_mmsi varchar(255);
@@ -48,9 +83,9 @@ begin
    fetch cargos into v_idCargoManifest;
    exit when cargos%notfound;
    
-   Select id into v_phases
-   from CargoManifestLoad
-   where v_idCargoManifest=ID; 
+   Select phasesId into v_phases
+   from CargoManifestUnload
+   where v_idCargoManifest=phasescargomanifestloadid; 
    
    Select realArrivalDate into v_arrivalDate_aux
    from phases

35e825afa92065a046b8b858e98ea3b2753ed58a
FranciscoRedol <1201239@isep.ipp.pt>
2021-12-27 00:06:55 +0000

    G42-179 #time 2h #comment Small corrections needed. US313 almost concluded

--- docs/US313/main.c
+++ docs/US313/main.c
@@ -1,20 +1,28 @@
 #include <stdio.h>
 #include "importFile.h"
 
-char shipAllocation [18][26][10][25];
-
 int main(){
 
-    //zeroArray();
-    importFile("cargoManifestShort.csv", shipAllocation);
+    char shipAllocation [18][26][10][25];
 
-    //for(int i = 0; i < 18; i++){
-        //for(int j = 0; j < 26; j++){
-            //for(int k = 0; k < 10; k++){
-                //printf("i: %d j: %d k: %d tem navio : %s\n", i, j, k, shipAllocation[i][j][k]);
-            //}
-        //}
-    //}
+    for(int i = 0; i < 18; i++){
+        for(int j = 0; j < 26; j++){
+            for(int k = 0; k < 10; k++){
+                *shipAllocation[i][j][k] = '\0';
+            }
+        }
+    }
+
+    importFile("cargoManifest.csv", shipAllocation);
+
+    for(int i = 0; i < 18; i++){
+        for(int j = 0; j < 26; j++){
+            for(int k = 0; k < 10; k++){
+                if(shipAllocation[i][j][k] != '\0')
+                    printf("x: %d y: %d z: %d tem contentor: %s\n", i, j, k, shipAllocation[i][j][k]);
+            }
+        }
+    }
 
     return 0;
 }

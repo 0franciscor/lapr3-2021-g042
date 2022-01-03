2624f5465cdf2c9f6b71d748394d16cb171f4cea
FranciscoRedol <1201239@isep.ipp.pt>
2022-01-03 12:50:56 +0000

    G42-182 #comment Bug fix.

--- docs/US316/printResult.c
+++ docs/US316/printResult.c
@@ -5,7 +5,7 @@ void printResult(char resultado, int numEnviados){
     char *outputFileName = "US316.csv";
     FILE *outputFile = fopen(outputFileName, "a");
 
-    fprintf(outputFile, "Out of the %d inserted Containers, %d were found.\n", resultado, numEnviados);
+    fprintf(outputFile, "Out of the %d inserted Containers, %d were found.\n", numEnviados, resultado);
     fprintf(outputFile, "\n");
 
     fclose(outputFile);

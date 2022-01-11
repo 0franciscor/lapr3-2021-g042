5a13b913b8740cc6778f877a8596113e9cb28ffb
FranciscoRedol <1201239@isep.ipp.pt>
2022-01-11 16:29:18 +0000

    G42-240 #comment main.c  updates for dynamic memmory allocation.

--- docs/US409/main.c
+++ docs/US409/main.c
@@ -11,10 +11,12 @@ typedef struct { //define-se a estrutura cMContainer
     int x;
     int y;
     int z;
-    int grossContainer;
+    float grossContainer;
     int phases_cMLoadId;
-    int cMUnloadI
+    int cMUnloadI;
 } cMContainer;
+
+struct cmContainer ***containerArray;
 
 int main(){
 
@@ -28,21 +30,45 @@ int main(){
 
     int maxX, maxY, maxZ;
     fscanf(containerFile, "%d,%d,%d", &maxX, &maxY, &maxZ);
-    int shipAllocation[maxX][maxY][maxZ];
 
-    zeroArray(maxX, maxY, maxZ, shipAllocation);
+    containerArray = (struct cmContainer ***) calloc(maxX, sizeof(struct cmContainer***));
+    
+    if(containerArray == NULL){
+        printf("Error reserving memory(x).\n");
+        exit (1);
+    }
 
-    while(!feof(containerFile)){ //leitura de cada linha do ficheiro
+    for (int i = 0; i < maxY; i++){
+        containerArray[i] = (struct cmContainer **) calloc(maxY , sizeof(struct cmContainer**)); 
+        
+        if(containerArray[i] == NULL){
+            printf("Error reserving memory(y).\n");
+            exit (1);
+        }
+
+        for(int k = 0; k < maxZ; k++){
+            containerArray[i][k] = (struct cmContainer *) calloc (maxZ , sizeof(struct cmContainer*)); 
+
+            if(containerArray[i][k] == NULL){
+                printf("Error reserving memory(z).\n");
+                exit (1);
+            }
+        }
+    }
+
+    zeroArray(maxX, maxY, maxZ, containerArray);
+
+    /*while(!feof(containerFile)){ //leitura de cada linha do ficheiro
         int x, y, z, containerID;
 
         fscanf(containerFile, "%d,%d,%d,%d", &containerID, &x, &y, &z);
 
         shipAllocation[x][y][z] = containerID;
-    }
+    }*/
 
     fclose(containerFile);
 
-    printArray(maxX,maxY,maxZ, shipAllocation);
+    //printArray(maxX,maxY,maxZ, containerArray);
             
     return 0;
 }

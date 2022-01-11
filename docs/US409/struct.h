0f38fc1ef23daaee1b1d9cd6c2e8171e533c6891
FranciscoRedol <1201239@isep.ipp.pt>
2022-01-11 17:25:22 +0000

    G42-322 #time 2h #comment Struct defined in separate .h file

---
+++ docs/US409/struct.h
@@ -0,0 +1,11 @@
+typedef struct { //define-se a estrutura cMContainer
+    int cMLoadId;
+    int containerId;
+    int phasesId;
+    int x;
+    int y;
+    int z;
+    float grossContainer;
+    int phases_cMLoadId;
+    int cMUnloadI;
+} cMContainer;

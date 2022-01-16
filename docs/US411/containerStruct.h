0574538e2f25163317c1af6e84b49e7318b9ab24
FranciscoRedol <1201239@isep.ipp.pt>
2022-01-16 03:44:49 +0000

    G42-242 #time 8h Development of US's core.

---
+++ docs/US411/containerStruct.h
@@ -0,0 +1,13 @@
+typedef struct { //cMContainer struct
+    int containerId;                       
+    int x;
+    int y;
+    int z;
+    char isRefrigerated;
+    int isoCode;
+    float grossContainer;
+    float maxWeight;
+    float weight;
+    float tare;
+    float maxVolume;
+} cMContainer;

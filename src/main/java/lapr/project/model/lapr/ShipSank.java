a6b79f84027702469dfbd1539c07af563e378b65
pedroccelestino <1201382@isep.ipp.pt>
2022-01-21 22:34:13 +0000

    G42-334 #time 4h #comment US420 - Changes on Implementation and Tests

--- src/main/java/lapr/project/model/lapr/ShipSank.java
+++ src/main/java/lapr/project/model/lapr/ShipSank.java
@@ -1,64 +1,82 @@
 package lapr.project.model.lapr;
 
+import lapr.project.model.Ship;
+
 public class ShipSank {
 
-    private final double DENSITY = 1.03;
-    private final double CONTAINER_MASS = 0.5;
+    private final double DENSITY = 1030.0;
+    private final double CONTAINER_MASS = 500.0;
+    private final double GRAVITY = 9.8;
 
-    private double vesselEmptyVolume;
-    private double vesselLoadedVolume;
+    private double vesselImmersedVolumeEmpty;
+    private double vesselImmersedVolumeLoaded;
     private double shipSankHeight;
-
+    private double weightForce;
 
     public ShipSank(){
-        this.vesselEmptyVolume = 0;
-        this.vesselLoadedVolume = 0;
+        this.vesselImmersedVolumeEmpty = 0;
+        this.vesselImmersedVolumeLoaded = 0;
         this.shipSankHeight = 0;
+        this.weightForce = 0;
     }
-    /**
-     * This method returns the volume of the Vessel without conteiners.
-     * @param vesselMass
-     * @param containers
-     * @return Vessel Volume with 0 containers
-     */
-    public double vesselVolume (double vesselMass, int containers){
+
+    
+    public double shipMass(double vesselMass, int containers){
+        return (vesselMass + (containers * CONTAINER_MASS));
+    }
+
+
+    public double vesselImmersedVolume (double vesselMass, int containers){
         double volume;
         volume = (vesselMass + (containers * CONTAINER_MASS))/DENSITY;
         return volume;
     }
 
-    /**
-     * This method returns the Height the Vessel sank when the containers are loaded.
-     * @param vesselMass
-     * @param width
-     * @param length
-     * @param containers
-     * @return Sank Height
-     */
-    public void shipSankHeight (double vesselMass, double width, double length, int containers) {
-        double vesselEmptyVolume, vesselLoadedVolume, initialHeight, finalHeight;
-
-        vesselEmptyVolume = vesselVolume(vesselMass,0);
-        this.vesselEmptyVolume = vesselEmptyVolume;
-        vesselLoadedVolume = vesselVolume(vesselMass,containers);
-        this.vesselLoadedVolume = vesselLoadedVolume;
-
-        initialHeight = vesselEmptyVolume / (length * width);
-        finalHeight = vesselLoadedVolume / (length * width);
-        this.shipSankHeight = (initialHeight - finalHeight);
+    public double calculateDraft (Ship ship, double volume) {
+        double draft;
+        draft = (volume / (ship.getLength() * ship.getWidth()));
+        return draft;
     }
 
-    public double getVesselEmptyVolume() {
-        return vesselEmptyVolume;
+    public void shipSankHeight (Ship ship, double vesselMass, int containers) {
+        double vesselImmersedVolume, vesselImmersedVolumeLoaded, emptyDraft, loadedDraft;
+
+        vesselImmersedVolume = vesselImmersedVolume(vesselMass,0);
+        this.vesselImmersedVolumeEmpty = vesselImmersedVolume;
+        vesselImmersedVolumeLoaded = vesselImmersedVolume(vesselMass,containers);
+        this.vesselImmersedVolumeLoaded = vesselImmersedVolumeLoaded;
+
+        emptyDraft = calculateDraft(ship,vesselImmersedVolumeEmpty);
+        loadedDraft = calculateDraft(ship,vesselImmersedVolumeLoaded);
+
+        this.shipSankHeight = loadedDraft - emptyDraft;
     }
-    public double getVesselLoadedVolume() {
-        return vesselLoadedVolume;
+
+    public double pressureExertedByVesselOnWater(Ship ship, double vesselMass, int containers){
+        double weightForce, pressure;
+
+        weightForce = calculateWeightForce(shipMass(vesselMass,containers));
+        pressure = calculatePressure(ship,weightForce);
+
+        return pressure;
     }
+
+
+    public double calculateWeightForce(double vesselMass){
+        double weightForce = ((vesselMass * GRAVITY));
+        this.weightForce = weightForce;
+        return weightForce;
+    }
+
+    public double calculatePressure(Ship ship, double weightForce){
+        double pressure = (weightForce / (ship.getLength()*ship.getWidth()));
+        return pressure;
+    }
+
+
     public double getShipSankHeight() {
         return shipSankHeight;
     }
-
-
 
 
 
--- src/test/java/lapr/project/model/lapr/ShipSankTest.java
+++ src/test/java/lapr/project/model/lapr/ShipSankTest.java
@@ -1,6 +1,10 @@
 package lapr.project.model.lapr;
 
+import lapr.project.model.Ship;
+import lapr.project.model.ShipLocation;
 import org.junit.jupiter.api.Test;
+
+import java.util.Date;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -8,30 +12,130 @@ class ShipSankTest {
 
     private ShipSank shipSank;
 
-
     @Test
-    public void vesselVolumeUnloaded() {
+    public void pressureExertedByVesselOnWater() {
+        Date dateToUse = new Date();
+        ShipLocation shipl = new ShipLocation("111111111", dateToUse, "56", "88", 32f, 44f, "330", "B");
         shipSank = new ShipSank();
-        shipSank.shipSankHeight(152*Math.pow(10, 6), 42, 342, 0);
-        shipSank.getVesselEmptyVolume();
-        shipSank.getVesselLoadedVolume();
-        shipSank.getShipSankHeight();
+        Ship ship1 = new Ship("111111111", "SHIP1", "IMO1111111", 4, 200.0f, "REEFER", 70, 342f, 42f, "ASD", 24f, shipl);
 
-        double result = shipSank.getVesselEmptyVolume();
-        double expected = 1.4757281553398058 * Math.pow(10, 8);
+        double result = shipSank.pressureExertedByVesselOnWater(ship1, 152 * Math.pow(10, 6), 20000);
+        System.out.println(result);
+        double expected = 110526.31578947368;
         assertEquals(result, expected);
     }
 
     @Test
-    public void vesselVolumeLoaded() {
+    public void calculateWeightForce () {
+        Date dateToUse = new Date();
+        ShipLocation shipl = new ShipLocation("111111111", dateToUse, "56", "88", 32f, 44f, "330", "B");
         shipSank = new ShipSank();
-        shipSank.shipSankHeight(152*Math.pow(10, 6), 42, 342, 10000);
-        shipSank.getVesselEmptyVolume();
-        shipSank.getVesselLoadedVolume();
-        shipSank.getShipSankHeight();
+        Ship ship1 = new Ship("111111111", "SHIP1", "IMO1111111", 4, 200.0f, "REEFER", 70, 342f, 42f, "ASD", 24f, shipl);
 
-        double result = shipSank.getVesselEmptyVolume();
-        double expected = 1.4757281553398058 * Math.pow(10, 8);
+        double result = shipSank.calculateWeightForce(152 * Math.pow(10, 6));
+        double expected = 1.4896E9;
+        assertEquals(result, expected);
+    }
+
+    @Test
+    public void calculatePressure() {
+        Date dateToUse = new Date();
+        ShipLocation shipl = new ShipLocation("111111111", dateToUse, "56", "88", 32f, 44f, "330", "B");
+        shipSank = new ShipSank();
+        Ship ship1 = new Ship("111111111", "SHIP1", "IMO1111111", 4, 200.0f, "REEFER", 70, 342f, 42f, "ASD", 24f, shipl);
+
+        double result = shipSank.calculatePressure(ship1, shipSank.calculateWeightForce(152 * Math.pow(10, 6)));
+        double expected = 103703.70370370371;
+        assertEquals(result, expected);
+    }
+
+    @Test
+    public void shipMass(){
+        Date dateToUse = new Date();
+        ShipLocation shipl = new ShipLocation("111111111", dateToUse, "56", "88", 32f, 44f, "330", "B");
+        shipSank = new ShipSank();
+        Ship ship1 = new Ship("111111111", "SHIP1", "IMO1111111", 4, 200.0f, "REEFER", 70, 342f, 42f, "ASD", 24f, shipl);
+
+        double result = shipSank.shipMass(152 * Math.pow(10, 6),20000);
+        double expected = 1.62E8;
+        assertEquals(result, expected);
+    }
+
+    @Test
+    public void vesselImmersedVolumeWithZeroContainers() {
+        Date dateToUse = new Date();
+        ShipLocation shipl = new ShipLocation("111111111", dateToUse, "56", "88", 32f, 44f, "330", "B");
+        shipSank = new ShipSank();
+        Ship ship1 = new Ship("111111111", "SHIP1", "IMO1111111", 4, 200.0f, "REEFER", 70, 342f, 42f, "ASD", 24f, shipl);
+
+        double result = shipSank.vesselImmersedVolume(152 * Math.pow(10, 6),0);
+        double expected = 147572.81553398058;
+        assertEquals(result, expected);
+    }
+
+    @Test
+    public void vesselImmersedVolumeWith20kContainers() {
+        Date dateToUse = new Date();
+        ShipLocation shipl = new ShipLocation("111111111", dateToUse, "56", "88", 32f, 44f, "330", "B");
+        shipSank = new ShipSank();
+        Ship ship1 = new Ship("111111111", "SHIP1", "IMO1111111", 4, 200.0f, "REEFER", 70, 342f, 42f, "ASD", 24f, shipl);
+
+        double result = shipSank.vesselImmersedVolume(152 * Math.pow(10, 6),20000);
+        double expected = 157281.55339805825;
+        assertEquals(result, expected);
+    }
+
+    @Test
+    public void calculateDraftVesselEmpty(){
+        Date dateToUse = new Date();
+        ShipLocation shipl = new ShipLocation("111111111", dateToUse, "56", "88", 32f, 44f, "330", "B");
+        shipSank = new ShipSank();
+        Ship ship1 = new Ship("111111111", "SHIP1", "IMO1111111", 4, 200.0f, "REEFER", 70, 342f, 42f, "ASD", 24f, shipl);
+
+        double immersedVolume = shipSank.vesselImmersedVolume(152 * Math.pow(10, 6),0);
+        double result = shipSank.calculateDraft(ship1,immersedVolume);
+        double expected = 10.273796681563672;
+        assertEquals(result, expected);
+    }
+
+    @Test
+    public void calculateDraftVesselWith20kContainers(){
+        Date dateToUse = new Date();
+        ShipLocation shipl = new ShipLocation("111111111", dateToUse, "56", "88", 32f, 44f, "330", "B");
+        shipSank = new ShipSank();
+        Ship ship1 = new Ship("111111111", "SHIP1", "IMO1111111", 4, 200.0f, "REEFER", 70, 342f, 42f, "ASD", 24f, shipl);
+
+        double immersedVolume = shipSank.vesselImmersedVolume(152 * Math.pow(10, 6),20000);
+        double result = shipSank.calculateDraft(ship1,immersedVolume);
+        double expected = 10.949704357982334;
+        assertEquals(result, expected);
+    }
+
+    @Test
+    public void shipSankHeigth(){
+        Date dateToUse = new Date();
+        ShipLocation shipl = new ShipLocation("111111111", dateToUse, "56", "88", 32f, 44f, "330", "B");
+        shipSank = new ShipSank();
+        Ship ship1 = new Ship("111111111", "SHIP1", "IMO1111111", 4, 200.0f, "REEFER", 70, 342f, 42f, "ASD", 24f, shipl);
+
+        double immersedVolumeEmpty = shipSank.vesselImmersedVolume(152 * Math.pow(10, 6),0);
+        double immersedVolumeLoaded = shipSank.vesselImmersedVolume(152 * Math.pow(10, 6),20000);
+        double emptyDraft = shipSank.calculateDraft(ship1,immersedVolumeEmpty);
+        double loadedDraft = shipSank.calculateDraft(ship1,immersedVolumeLoaded);
+        double result = loadedDraft - emptyDraft;
+        double expected = 0.675907676418662;
+        assertEquals(result, expected);
+    }
+
+    @Test
+    public void getShipSankHeight() {;
+        Date dateToUse = new Date();
+        ShipLocation shipl = new ShipLocation("111111111", dateToUse, "56", "88", 32f, 44f, "330", "B");
+        shipSank = new ShipSank();
+        Ship ship1 = new Ship("111111111", "SHIP1", "IMO1111111", 4, 200.0f, "REEFER", 70, 342f, 42f, "ASD", 24f, shipl);
+        shipSank.shipSankHeight(ship1,152 * Math.pow(10, 6),20000);
+        double result = shipSank.getShipSankHeight();
+        double expected = 0.675907676418662;
         assertEquals(result, expected);
     }
 }

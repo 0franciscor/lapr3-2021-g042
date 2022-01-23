b688f86952c4064a8450f1cca8af0e59ed519499
Rita Ariana Sobral <1201386@isep.ipp.pt>
2022-01-23 17:48:22 +0000

    G42-232 #time 4h #comment Relatório

--- src/main/java/lapr/project/controller/AddContainersWithoutChangingComController.java
+++ src/main/java/lapr/project/controller/AddContainersWithoutChangingComController.java
@@ -1,7 +1,9 @@
 package lapr.project.controller;
 
 import lapr.project.model.CentroMassa;
+import lapr.project.utils.WriteForAFile;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -17,6 +19,12 @@ public class AddContainersWithoutChangingComController {
         double centroMassaXPosContentor = CentroMassa.adicionarContentoresSemAlterarCentroMassaX(numeroPartes,massaTotal,xInicial,xFinal,yInicial,yFinal,formasGeometricas,massaAcrescentar, centroMassaX);
         double centroMassaYPosContentor = CentroMassa.adicionarContentoresSemAlterarCentroMassaY(numeroPartes,massaTotal,xInicial,xFinal,yInicial,yFinal,formasGeometricas,massaAcrescentar, centroMassaY);
 
+        WriteForAFile writeForAFile = new WriteForAFile();
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("Coordinates of the Container's Center of Mass\n");
+        stringBuilder.append("->X: " + String.format("%.2f",centroMassaXPosContentor) + " m\n");
+        stringBuilder.append("->Y: " +  String.format("%.2f",centroMassaYPosContentor) + " m\n");
+        writeForAFile.writeForAFile(stringBuilder.toString(), "US419_CentroDeMassa", new File(".\\outputs\\US419"), false);
         List<Double> centroDeMassaPosAdicionar = new ArrayList<>();
 
         centroDeMassaPosAdicionar.add(centroMassaXPosContentor);
